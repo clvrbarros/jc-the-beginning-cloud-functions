f3841815b893f6c1f68e89cfb13050730c7d98c8
joaoPSV <joao.p1@hotmail.com>
2019-06-25 23:53:17 -0300

    First commit

---
+++ functions/index.js
@@ -0,0 +1,36 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+exports.teste = functions.https.onCall((data, context) => {
+    return "Functionou fela da puta";
+});
+
+exports.sum = functions.https.onCall((data, context) => {
+    var number1 = data.number1 || 0;
+    var number2 = data.number2 || 0;
+    return number1 + number2;
+});
+
+exports.testingGet = functions.https.onRequest((req, res) => {
+    res.status(200).json({
+        description: "chupa meu pau!",
+        msg: "pequenino!"
+    });
+});
+
+exports.upOneNumber = functions.firestore
+    .document("/users/{userId}")
+    .onCreate(async (snapshot, context) => {
+        try{
+            await snapshot.ref.update({age: snapshot.data().age + 1});
+            return true;
+        }catch(e){
+            return e;
+        }
+    })
